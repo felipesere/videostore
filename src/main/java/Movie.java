46bca55370f77c2a07c109d2d1c0e9ac0de4ef5b
Felipe Sere <felipesere@gmail.com>
2014-09-19 09:59:01 +0100

    Move getAmount to RegularMovie and update tests

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -25,10 +25,6 @@ public class Movie {
 	}
 
   double getAmount(int daysRented) {
-    double amount = 2;
-    if (daysRented > 2) {
-      amount += (daysRented - 2) * 1.5;
-    }
-    return amount;
+     return 0;
   }
 }
--- src/main/java/RegularMovie.java
+++ src/main/java/RegularMovie.java
@@ -2,4 +2,12 @@ public class RegularMovie extends Movie {
   public RegularMovie(String title, int priceCode) {
     super(title, priceCode);
   }
+
+  double getAmount(int daysRented) {
+    double amount = 2;
+    if (daysRented > 2) {
+      amount += (daysRented - 2) * 1.5;
+    }
+    return amount;
+  }
 }
