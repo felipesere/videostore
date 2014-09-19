a086d5a7991caa2591523c99bc3ddc493a48fbca
Felipe Sere <felipesere@gmail.com>
2014-09-19 09:42:02 +0100

    move getAmount() to rental

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -23,7 +23,7 @@ public class Customer {
     String result = "Rental Record for " + getName() + "\n";
 
     for(Rental rental : rentals) {
-      double thisAmount = getAmount(rental);
+      double thisAmount = rental.getAmount();
 
       frequentRenterPoints++;
 
@@ -37,27 +37,5 @@ public class Customer {
     result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
 
     return result;
-  }
-
-  private double getAmount(Rental rental) {
-    double amount = 0;
-    switch (rental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        amount += 2;
-        if (rental.getDaysRented() > 2) {
-          amount += (rental.getDaysRented() - 2) * 1.5;
-        }
-        break;
-      case Movie.NEW_RELEASE:
-        amount += rental.getDaysRented() * 3;
-        break;
-      case Movie.CHILDRENS:
-        amount += 1.5;
-        if (rental.getDaysRented() > 3){
-          amount += (rental.getDaysRented() - 3) * 1.5;
-        }
-        break;
-    }
-    return amount;
   }
 }
