2b1eac3b312c2b506546dc72bde58f941a92f813
Felipe Sere <felipesere@gmail.com>
2014-09-19 10:15:18 +0100

    extract getFrequentRenterPoints with daysRented paramter

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -21,9 +21,13 @@ public class Rental {
   }
 
   int getFrequentRenterPoints() {
+    return getFrequentRenterPoints(daysRented);
+  }
+
+  private int getFrequentRenterPoints(int daysRented) {
     int frequentRenterPoints = 1;
 
-    if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1){
+    if (getMovie().getPriceCode() == Movie.NEW_RELEASE && daysRented > 1){
       frequentRenterPoints++;
     }
     return frequentRenterPoints;
